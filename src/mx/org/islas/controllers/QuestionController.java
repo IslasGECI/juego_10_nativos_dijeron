500c55ee5624dee0a54cd55221c4b3a8528515bb
Efren Cabrera <efren507.507@gmail.com>
2019-07-10 15:49:03 -0700

    Agrega parámetro de entrada para conocer que preguntas excluir

--- src/mx/org/islas/controllers/QuestionController.java
+++ src/mx/org/islas/controllers/QuestionController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import mx.org.islas.models.Question;
 
+import java.util.ArrayList;
+
 /**
  * Clase que se encarga de atender las peticiones de nuevas preguntas
  */
@@ -13,11 +15,12 @@ import mx.org.islas.models.Question;
 public class QuestionController {
 
     /**
-     * Método para regresar una pregunta cuando se haga la petición
+     * Método que regresar una pregunta cuando se haga la petición
      * @return JSON que representa una pregunta
      */
     @RequestMapping("/question/get")
-    public Question GetQuestion() {
+    public Question getQuestion(@RequestParam(value="exclude") ArrayList idToExclude) {
+        System.out.println(idToExclude);
         Question q = new Question(1, "¿Cuál es el animal más común en Natividad");
         q.addAnswer(new Answer("Pardela", 5));
         return q;
