cd7a8ec5ac60a374a8e5ca00d127d7ddd95d2ba3
Efren Cabrera <efren507.507@gmail.com>
2019-07-10 15:49:22 -0700

    Agrega método para conocer la cantidad de preguntas disponibles

--- src/mx/org/islas/controllers/QuestionController.java
+++ src/mx/org/islas/controllers/QuestionController.java
@@ -2,6 +2,7 @@ package mx.org.islas.controllers;
 
 import mx.org.islas.models.Answer;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import mx.org.islas.models.Question;
@@ -24,5 +25,14 @@ public class QuestionController {
         Question q = new Question(1, "¿Cuál es el animal más común en Natividad");
         q.addAnswer(new Answer("Pardela", 5));
         return q;
+    }
+
+    /**
+     * Método que regresa la cantidad de preguntas disponibles
+     * @return Cantidad de preguntas disponibles
+     */
+    @RequestMapping("/question/count")
+    public int getQuestionCount() {
+        return 1;
     }
 }
