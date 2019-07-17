d6d9634a3ac06359fba5a088d056ea87d021a39f
Efren Cabrera <efren507.507@gmail.com>
2019-07-17 13:44:48 -0700

    Agrega función que obtiene pregunta

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -3,8 +3,19 @@ $(window).on("load", function() {
     var numberOfQuestions;
     async function main() {
         numberOfQuestions = await getCount();
+        let firstQuestion = await getQuestion(actualQuestion);
     }
     /**
+     * Función que obtiene una pregunta
+     * @param {*} questionID es el numero de pregunta
+     */
+    function getQuestion(questionID) {
+        return new Promise(function(resolve, reject) {
+            $.get(`/question/get?idQuestion=${questionID}`, (questionJSON) => {
+                resolve(questionJSON);
+            });
+        });
+    }
      * Función que obtiene el número total de preguntas
      */
     function getCount() {
