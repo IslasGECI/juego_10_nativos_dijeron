420ec0ebfd26bd1acd43df4f9ba7bd80c045d5fd
Efren Cabrera <efren507.507@gmail.com>
2019-07-17 13:40:49 -0700

    Agrega función que obtiene el numero total de preguntas

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,1 +1,19 @@
-console.log("l");
+$(window).on("load", function() {
+
+    var numberOfQuestions;
+    async function main() {
+        numberOfQuestions = await getCount();
+    }
+    /**
+     * Función que obtiene el número total de preguntas
+     */
+    function getCount() {
+        return new Promise(function(resolve, reject) {
+            $.get(`/question/count`, (count) => {
+                resolve(count);
+            });
+        });
+    }
+
+    main();
+});
