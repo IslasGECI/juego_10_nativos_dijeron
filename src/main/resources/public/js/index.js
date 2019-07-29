2703f315ea8a1c26629a2c37ec9d00adf0348116
Fiorella <fiorellam@cetys.edu.mx>
2019-07-29 11:49:21 -0700

    Agrega funciones a nuevo archivo index.js
    Reduce la cantidad de líneas de código del archivo main.js distribuyendo algunas funciones al archivo index.js

---
+++ src/main/resources/public/js/index.js
@@ -0,0 +1,70 @@
+    var roundScore = 0;
+
+    /**
+     * Función que obtiene una pregunta
+     * @param {number} questionID es el numero de pregunta
+     */
+    function getQuestionByID(questionID) {
+        return new Promise(function(resolve, reject) {
+            $.get(`/question/get?idQuestion=${questionID}`, (questionJSON) => {
+                resolve(questionJSON);
+            });
+        });
+    }
+    /**
+     * Función que obtiene el número total de preguntas
+     */
+    function getQuestionCount() {
+        return new Promise(function(resolve, reject) {
+            $.get(`/question/count`, (count) => {
+                resolve(count);
+            });
+        });
+    }
+
+    /**
+     * Función que inicializa los puntajes de los tableros
+     */
+    function setInitialScore2Boards(){
+        $("#center_score").html(`<p>${roundScore}</p>`);
+        $("#left_score").html(`<p>${roundScore}</p>`);
+        $("#right_score").html(`<p>${roundScore}</p>`);
+    }
+    /**
+     * Función que reproduce los sonidos
+     * @param {string} audioPath es la ruta en la que se encuentra el audio por reproducir
+     */
+    function playSound(audioPath){
+        var sound = new Audio (audioPath);
+        sound.play();
+    }
+
+    /**
+     * Función que agrega clase hide que se encuentra en el css
+     * @param {string} element elemento al que se le quiere agregar clase hide
+     */
+    function addHideClass(element){
+        for (let i=1; i<6; i++){
+            $(`#${element}${i}`).addClass("hide");
+        }
+    }
+    /**
+     * Función que remueve clase hide que se encuentra en el css
+     * @param {string} element elemento de html al que se le quiere remover la clase hide
+     */
+    function removeHideClass(element){
+        for (let i=1; i<6; i++){
+            $(`#${element}${i}`).removeClass("hide");
+        }
+    }
+
+    /**
+     * Función que agrega listener a botón de respuesta incorrecta muestra imagen y reproduce sonido
+     */
+    function showErrorAnswerMessage(){
+        $("#wrong_answer").click(function (){
+            $("#error_image").show(1).delay(800).hide(1);
+            var wrongAudio = ("sound/wrong-answer.mp3");
+            playSound(wrongAudio);
+        });
+    }
--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -31,27 +31,7 @@ $(window).on("load", function() {
         }
         $("#next_question").addClass("hide");
     }
-    /**
-     * Función que obtiene una pregunta
-     * @param {*} questionID es el numero de pregunta
-     */
-    function getQuestionByID(questionID) {
-        return new Promise(function(resolve, reject) {
-            $.get(`/question/get?idQuestion=${questionID}`, (questionJSON) => {
-                resolve(questionJSON);
-            });
-        });
-    }
-    /**
-     * Función que obtiene el número total de preguntas
-     */
-    function getQuestionCount() {
-        return new Promise(function(resolve, reject) {
-            $.get(`/question/count`, (count) => {
-                resolve(count);
-            });
-        });
-    }
+    
 
     /**
      * Función que agrega audio al seleccionar un botón de respuesta y muestra el 
@@ -112,38 +92,24 @@ $(window).on("load", function() {
         showNextQuestionButton();
     }
 
-    /**
-     * Función que agrega listener a botón de respuesta incorrecta
-     * muestra imagen y reproduce sonido
-     */
-    function showErrorAnswerMessage(){
-        $("#wrong_answer").click(function (){
-            $("#error_image").show(1).delay(800).hide(1);
-            var wrongAudio = ("sound/wrong-answer.mp3");
-            playSound(wrongAudio);
-        });
-    }
 
     /**
      * Función que ordena las respuestas de las preguntas de acuerdo a la cantidad de personas que 
      * dijeron una respuesta
      * @param {question} question es la pregunta a la que se le ordenarán las respuestas
      */
-    function addHideClass(element){
-        for (let i=1; i<6; i++){
-            $(`#${element}${i}`).addClass("hide");
-        }
+    function orderAnswers(question){
+        return question.answers.sort((a,b) => {
+            if ( a.count < b.count ){
+                return 1;
+            }
+            if ( a.count > b.count ){
+                return -1;
+            }
+                return 0;
+        });
     }
-    /**
-     * Función que remueve clase hide que se encuentra en el css
-     * @param {*} element elemento de html al que se le quiere
-     * remover la clase hide
-     */
-    function removeHideClass(element){
-        for (let i=1; i<6; i++){
-            $(`#${element}${i}`).removeClass("hide");
-        }
-    }
+
     /**
      * Función que  muestra el botón de siguiente pregunta
      */
@@ -161,41 +127,6 @@ $(window).on("load", function() {
             $("#next_question").addClass("hide");
         }
     }
-    /**
-     * Función que ordena las respuestas de las preguntas de acuerdo 
-     * a la cantidad de personas que dijeron una respuesta
-     * @param {*} question es la pregunta a la que se le ordenarán
-     * las respuestas
-     */
-    function orderAnswers(question){
-        return question.answers.sort((a,b) => {
-            if ( a.count < b.count ){
-                return 1;
-            }
-            if ( a.count > b.count ){
-                return -1;
-            }
-                return 0;
-        });
-    }
-    /**
-     * Función que inicializa los puntajes de los tableros
-     */
-    function setInitialScore2Boards(){
-        $("#center_score").html(`<p>${roundScore}</p>`);
-        $("#left_score").html(`<p>${roundScore}</p>`);
-        $("#right_score").html(`<p>${roundScore}</p>`);
-    }
-
-    /**
-     * Función que reproduce los sonidos
-     * @param {*} audioPath es la ruta en la que se encuentra 
-     * el audio por reproducir
-     */
-    function playSound(audioPath){
-        var sound = new Audio (audioPath);
-        sound.play();
-    }
-
+    
     main();
 });
