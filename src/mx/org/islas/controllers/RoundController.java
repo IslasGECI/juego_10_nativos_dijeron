de322ac2464eb5b9f8b281c84416b75c17fcf291
Fiorella <fiorellam@cetys.edu.mx>
2019-10-01 12:03:29 -0700

    Agrega controlador de ronda

---
+++ src/mx/org/islas/controllers/RoundController.java
@@ -0,0 +1,36 @@
+package mx.org.islas.controllers;
+
+import mx.org.islas.data.ExcelReader;
+import mx.org.islas.models.Question;
+import mx.org.islas.models.Round;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Clase que se encarga de atender las peticiones de preguntas de un round
+ */
+@RestController
+public class RoundController {
+
+    public static final String FILE_PATH = "src/survey.xlsx";
+
+    /**
+     * Método que regresa la información de una ronda
+     */
+    @RequestMapping("rounds/questions/")
+    public ArrayList<Round> getRounds() throws IOException {
+        ArrayList<Question> questions = ExcelReader.loadQuestions(FILE_PATH);
+        ArrayList<Round> rounds = ExcelReader.populateRound(questions);
+        return rounds;
+    }
+    @RequestMapping("round/questions/")
+    public Round getQuestions() throws IOException {
+        ArrayList<Question> questions = ExcelReader.loadQuestions(FILE_PATH);
+        Round round = new Round (questions.size(), questions);
+        return round;
+    }
+}
